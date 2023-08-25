440d9fd6e6eda49153d7b5d2b2923488a8c5ff8c
Emiliano Rodriguez <31290912+Emiliano-Rodriguez@users.noreply.github.com>
2023-08-25 11:04:13 -0500

    Add files via upload

---
+++ scripts/charts_mobile.js
@@ -0,0 +1,39 @@
+new TradingView.MediumWidget(
+  {
+  "symbols": [
+    [
+      "S&P 500",
+      "FOREXCOM:SPXUSD"
+    ],
+    [
+      "APPLE",
+      "AAPL"
+    ],
+    [
+      "ETH",
+      "BITSTAMP:ETHUSD"
+    ],
+    [
+      "BTC",
+      "BITSTAMP:BTCUSD"
+    ]
+  ],
+  "chartOnly": false,
+  "width": 400,
+  "height": 400,
+  "locale": "en",
+  "colorTheme": "dark",
+  "gridLineColor": "rgba(240, 243, 250, 0)",
+  "fontColor": "#787B86",
+  "isTransparent": true,
+  "autosize": false,
+  "showVolume": false,
+  "scalePosition": "no",
+  "scaleMode": "Normal",
+  "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+  "noTimeScale": true,
+  "valuesTracking": "1",
+  "chartType": "line",
+  "container_id": "tradingview_b4826"
+}
+  );
